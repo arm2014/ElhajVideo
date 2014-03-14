0a4715e302b41d1feedd549df18db6413f941dce
esmail <eng.mohamed.au@gmail.com>
2014-03-15 01:53:01 +0200

    First

---
+++ src/com/mohamed/elhajvideo/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.mohamed.elhajvideo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash);
+	
+	}
+}
