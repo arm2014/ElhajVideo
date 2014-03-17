f97ce9b855fbaf88099df9c50f1100e773179f0d
esmail <eng.mohamed.au@gmail.com>
2014-03-17 04:35:51 +0200

    save new

--- src/com/mohamed/elhajvideo/SplashActivity.java
+++ src/com/mohamed/elhajvideo/SplashActivity.java
@@ -1,15 +1,30 @@
 package com.mohamed.elhajvideo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends Activity {
 
+	public static final int SPLASH_DELAYED=3000;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
+		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash);
+		new Handler().postDelayed(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+		
+				Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+				finish();
+				startActivity(intent);
+			}
+		}, SPLASH_DELAYED);
 	
 	}
 }
